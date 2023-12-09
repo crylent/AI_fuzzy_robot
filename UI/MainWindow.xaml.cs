294e3cc68a11b3197e032c4de35816c0d8429dfb
Somnie Maxim Belkovsky <inkerimaa1@gmail.com>
2023-12-09 18:47:12 +0300

    Minor UI improvement

--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Media;
 
 namespace AI_robot.UI
 {
@@ -7,23 +8,38 @@ namespace AI_robot.UI
     /// </summary>
     public partial class MainWindow
     {
+        private readonly Brush _pressedBackground = Brushes.Goldenrod;
+        private readonly Brush _defaultBackground;
+        
         public MainWindow()
         {
             InitializeComponent();
+            _defaultBackground = RobotButton.Background;
+        }
+
+        private void ResetButtons()
+        {
+            ObstacleButton.Background = _defaultBackground;
+            RobotButton.Background = _defaultBackground;
         }
 
         private void OnClickObstacleButton(object sender, RoutedEventArgs e)
         {
+            ResetButtons();
+            ObstacleButton.Background = _pressedBackground;
             FieldCell.SetOnClickAction(FieldCell.OnClickAction.SetObstacle);
         }
 
         private void OnClickRobotButton(object sender, RoutedEventArgs e)
         {
+            ResetButtons();
+            RobotButton.Background = _pressedBackground;
             FieldCell.SetOnClickAction(FieldCell.OnClickAction.PutRobot);
         }
 
         private void OnClickPlayButton(object sender, RoutedEventArgs e)
         {
+            ResetButtons();
             FieldCell.SetOnClickAction(FieldCell.OnClickAction.None);
             Field.Field.Robot.MakeMove();
         }
